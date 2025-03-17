b2454635cf4e05232f717be256f427ea1b0d16ff
Atul Keshari <atulkeshari138@gmail.com>
2025-03-18 02:09:51 +0530

    Added delete airplane api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -37,5 +37,15 @@ async function getAirplane(req, res) {
     return res.status(error.statusCode).json(ErrorResponse);
   }
 }
+async function destroyAirplane(req, res) {
+  try {
+    const airplanes = await AirplaneService.destroyAirplane(req.params.id);
+    SuccessResponse.data = airplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
 
-module.exports = { createAirplane,getAirplanes,getAirplane};
+module.exports = { createAirplane, getAirplanes, getAirplane, destroyAirplane };
